6f3c851f8ca4ff66491ca51b281e5e07a7fb3d62
Hanif Warren <hwarrendev@gmail.com>
2018-04-25 03:23:50 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(thisArray, thisElement){
+  return [thisElement, ...thisArray]; 
+}
+
+function destructivelyAddElementToBeginningOfArray(thisArray, thisElement){
+  thisArray.unshift(thisElement);
+  return thisArray;
+}
+
+function addElementToEndOfArray(thisArray, thisElement){
+  return [...thisArray, thisElement];
+}
+
+function destructivelyAddElementToEndOfArray(thisArray, thisElement){
+thisArray.push(thisElement);
+return thisArray;
+}
+
+function accessElementInArray(thisArray, thisIndex){
+  return thisArray[thisIndex] ;
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(thisArray){
+    thisArray.shift();
+    return thisArray;
+  }
+  
+  function removeElementFromBeginningOfArray(thisArray){
+    thisArray = thisArray.slice(1);
+    return thisArray;
+  }
+  function destructivelyRemoveElementFromEndOfArray(thisArray){
+    thisArray.pop();
+    return thisArray;
+  }
